418e0ed62e46210431d54eed863f2f3c94b8663d
escutaceppat-dot <escutaceppat@gmail.com>
2026-04-22 19:39:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ function setupForm() {
     statusEl.className = '';
 
     try {
-      const res = await fetch('/api/submit', {
+      const res = await fetch('/api/send-email', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
